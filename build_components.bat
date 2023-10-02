9effd8e14400b061b495d4bc12846360b20abd70
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-10-02 14:28:41 +0100

    Update build_components.bat

--- build_components.bat
+++ build_components.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 cd dev
-start " " "C:\Program Files\Rhino 7\System\Rhino.exe" /nosplash /runscript="-_grasshopper _editor _load _document _open C:\Users\James\Documents\Rhino\Grasshopper\sDNA\source\repos\GHsDNAv0.01\dev\sDNA_build_components.gh _enter _exit _enterend"
+set REPO_PATH=%~dp0
+start " " "C:\Program Files\Rhino 7\System\Rhino.exe" /nosplash /runscript="-_grasshopper _editor _load _document _open %REPO_PATH%\dev\sDNA_build_components.gh _enter _exit _enterend"
+REM start " " "C:\Program Files\Rhino 7\System\Rhino.exe" /nosplash /runscript="-_grasshopper _editor _load _document _open C:\Users\James\Documents\Rhino\Grasshopper\sDNA\source\repos\GHsDNAv0.01\dev\sDNA_build_components.gh _enter _exit _enterend"
 cd ..
