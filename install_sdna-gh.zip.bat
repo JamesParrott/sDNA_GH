ce5a6e3501ed609375cf0b8466c661359c20ec0f
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2023-12-05 17:02:41 +0000

    Add comments about switches.

--- install_sdna-gh.zip.bat
+++ install_sdna-gh.zip.bat
@@ -2,8 +2,10 @@
 
 set user_objects_dir=%appdata%\Grasshopper\UserObjects\
 
+@REM copy /Y - overwrites dest if necessary
 copy /Y sdna-gh.zip %user_objects_dir%
 cd %user_objects_dir%
+@REM rem /S - recursive, /Q - Quietly, i.e. deletes without Y/N prompt
 rmdir /S /Q sdna-gh
 mkdir sdna-gh
 cd sdna-gh
