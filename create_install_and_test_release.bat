9ad0440918e08a06d34ac0df7948431e8fd0c2e6
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-05-21 18:49:20 +0100

    Build components as part of making release directly from repo, due to .bat difficulty!

--- create_install_and_test_release.bat
+++ create_install_and_test_release.bat
@@ -1,5 +1,4 @@
 @REM Cheetah_GH must be installed in the current Python env, or with pipx
-.\build_components.bat && ^
 .\create_release_sdna-gh.zip.bat && ^
 .\install_sdna-gh.zip.bat && ^
 Cheetah_GH .\src\sDNA_GH\Rhino_8_API_tests.gh NUM_TESTS 5
--- create_release_sdna-gh.zip.bat
+++ create_release_sdna-gh.zip.bat
@@ -3,16 +3,14 @@
 set repo_path=%~dp0
 set cwd=%CD%
 set zip_name=sdna-gh
+set target=%cwd%\%zip_name%
 
-mkdir %cwd%\%zip_name%
+mkdir %target%
 @REM pip will also install the deps into the target 
 @REM that've now been refactored own repos, 
 @REM and are now distributed via PyPi (no more static linking):
 @REM IronPyShp, toml_tools, Mapclassif-Iron, Cheetah_GH and Anteater_GH
-@REM
-@REM Requires there to be only a single sDNA_GH Wheel in \dist
-@REM e.g. from build_components.bat to have created 
-python -m pip install --target=%zip_name% %repo_path%\dist\sdna_gh-*-py2.py3-none-any.whl
+python -m pip install --target=%target% -e .
 
 @REM Zipping up the 'venv' (any directory pip installed into with --target)
 @REM is not a recommended distribution technique for Python libraries.
@@ -27,9 +25,9 @@ python -m pip install --target=%zip_name% %repo_path%\dist\sdna_gh-*-py2.py3-non
 @REM iv) while also allowing sDNA_GH to be installable and runnable (at
 @REM least in code) from Rhino 8 CPython 3 components, simply using the new auto magical syntax:  
 @REM #r: sDNA_GH
-cd %zip_name%
-tar -caf ..\%zip_name%.zip *
+cd %target%
+tar -caf %cwd%\%zip_name%.zip *
 
 @REM Clean up tmp build artefacts.
 cd %cwd%
-rmdir /s /q %cwd%\%zip_name%
+rmdir /s /q %target%
